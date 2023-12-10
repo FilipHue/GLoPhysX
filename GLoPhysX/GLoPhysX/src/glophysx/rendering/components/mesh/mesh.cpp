36fd2c882efbb045abc34fd92c3bbf65e743c1ba
FilipHue <filipomski@gmail.com>
2023-12-10 21:24:01 +0200

    Fixed sending the wrong data to the vertex and index buffer

--- GLoPhysX/GLoPhysX/src/glophysx/rendering/components/mesh/mesh.cpp
+++ GLoPhysX/GLoPhysX/src/glophysx/rendering/components/mesh/mesh.cpp
@@ -10,8 +10,8 @@ namespace GLOPHYSX {
 			Mesh::Mesh(std::vector<float>& vertices, std::vector<uint32_t>& indices, BufferLayout& layout)
 			{
 				m_vertex_array		= VertexArray::Create();
-				m_vertex_buffer		= VertexBuffer::Create(vertices.data(), sizeof(vertices.data()) * vertices.size());
-				m_index_buffer		= IndexBuffer::Create(indices.data(), sizeof(indices.data()) * indices.size());
+				m_vertex_buffer		= VertexBuffer::Create(vertices.data(), sizeof(*vertices.data()) * vertices.size());
+				m_index_buffer		= IndexBuffer::Create(indices.data(), indices.size());
 
 				m_vertex_buffer->SetLayout(layout);
 
